a65958bb2b3f3e2312124f9ac7cfb13e9477ed6b
Emre Ergeldi <bangfuzzy@gmail.com>
2022-12-05 13:30:44 +0300

    mobile app

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import { prisma } from "../db/prisma";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Category from "../components/Categories/Category";
 import Promotion from "../components/Promotions/Promotion";
-import { useWindowWidth } from "@react-hook/window-size";
+import MobileApp from "../components/MobileApp/MobileApp";
 
-export default function Home({ feed }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const windowWidth = useWindowWidth();
-
+export default function Home({
+  feed,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="w-screen h-screen">
       <Navbar />
-      {windowWidth <= 768 && <Promotion />}
       <SliderCard />
       <Category data={feed} />
-      {windowWidth > 768 && <Promotion />}
+      <Promotion />
+      <MobileApp />
     </div>
   );
 }
